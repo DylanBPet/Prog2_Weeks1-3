b72965bee6367452e6eb777e06484c36ee9e72be
DylanBPet <dylanpetroff13@gmail.com>
2026-01-23 19:41:00 -0500

    Week 4 readiongs. Made it to 4.4

---
+++ Assets/Scripts/Week 4 Readings/SpawningTanks.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpawningTanks : MonoBehaviour
+{
+    public GameObject tankPrefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Instantiate(tankPrefab, transform.position, Quaternion.identity);
+        }
+
+    }
+}
